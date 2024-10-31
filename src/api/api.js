dac5e145455b84916284d4c9415188c3869fbabd
mukando0115 <82659388+mukando0115@users.noreply.github.com>
2024-10-31 12:16:08 +0900

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // Server URL
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://hkz9nqql2m.execute-api.ap-northeast-2.amazonaws.com/api',
     headers: {
         "Content-Type": "application/json",
       },
