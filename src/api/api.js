9250bfbab6d7d02a383d37eeaa0c79fa8283299b
Myosotis <164124503+dnlgmldnjs@users.noreply.github.com>
2024-11-04 17:12:28 +0900

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Server URL
-    baseURL: 'https://zev4wu0r0a.execute-api.ap-northeast-2.amazonaws.com',
+    baseURL: 'https://zev4wu0r0a.execute-api.ap-northeast-2.amazonaws.com/api/',
     headers: {
         "Content-Type": "application/json",
       },
