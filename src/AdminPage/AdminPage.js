670c9e5d3f899f2ad59b8b06ad3cac8278500f43
mukando0115 <82659388+mukando0115@users.noreply.github.com>
2024-11-18 11:59:47 +0900

    Add files via upload

---
+++ src/AdminPage/AdminPage.js
@@ -0,0 +1,12 @@
+function AdminPage() {
+    return (
+        <main className="admin-page">
+            <div>
+                <p className="sub-title">Admin Page</p>
+                <h1 className="main-title">관리자페이지</h1>
+            </div>
+        </main>
+    )
+}
+
+export default AdminPage;
